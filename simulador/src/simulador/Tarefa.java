530d4f28f37b6ac78d596cdd6a768783f406adac
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-03-11 23:18:25 -0300

    Criado tarefa e agenda

--- simulador/src/simulador/Tarefa.java
+++ simulador/src/simulador/Tarefa.java
@@ -1,20 +1,26 @@
 package simulador;
 
+import java.util.function.Consumer;
+import java.util.function.DoubleUnaryOperator;
+
 /**
  * Created by fausto on 3/11/16.
  */
 public class Tarefa {
     private Double horario;
+    private Consumer<Double> funcao;
 
-    public Tarefa( Double horario){
+    public Tarefa(Double horario, Consumer<Double> funcao) {
         this.horario = horario;
+        this.funcao = funcao;
     }
 
     public Double getHorario() {
         return horario;
     }
 
-    public void setHorario(Double horario) {
-        this.horario = horario;
+    public void executar(){
+        this.funcao.accept(this.horario);
     }
+
 }
