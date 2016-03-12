ccf81487e9c99f1123f5564ca7e7adf2595d315f
unknown <victor.peres93@gmail.com>
2016-03-12 00:28:47 -0300

    Media e desvio padrão

--- simulador/src/simulador/Metricas.java
+++ simulador/src/simulador/Metricas.java
@@ -1,11 +1,27 @@
 package simulador;
 
+import java.util.List;
+
 /**
  * Created by Victor on 11.mar.2016.
  */
 public class Metricas {
+
     public static double Little(double lambda, double mi) {
         double rho = lambda/mi;
         return rho/(1-rho);
+    }
+
+    public static double Media(List<Double> lista) {
+        return lista.stream().mapToDouble(a->a).average().getAsDouble();
+    }
+
+    public static double DesvioPadrao(List<Double> lista) {
+        double desvioPadrao = 0;
+        double media = Media(lista);
+        for (Double item : lista) {
+            desvioPadrao += (item - media)*(item - media) / (lista.size() - 1);
+        }
+        return desvioPadrao;
     }
 }
